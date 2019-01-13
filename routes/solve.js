8d474e51836c44a3282f15c0a90fccabcfe800e1
Werner van Aswegen <wernerva@users.noreply.github.com>
2019-01-13 21:18:47 +0200

    Finished level 3

--- routes/solve.js
+++ routes/solve.js
@@ -2,26 +2,50 @@ const express = require('express');
 const router = express.Router();
 const level1 = require('../levels/level1');
 const level2 = require('../levels/level2');
+const level3 = require('../levels/level3');
 
-router.get('/:level', function (req, res) {
+router.get('/:level/:subLevel', function(req, res) {
     const level = req.params.level;
-    let solution1 = 'Not solved';
-    let solution2 = 'Not solved';
+    const subLevel = req.params.subLevel;
+    let solution = 'Not solved';
 
     switch (level) {
         case '01':
-            solution1 = level1.solve1();
-            solution2 = level1.solve2();
+            switch (subLevel) {
+                case '2':
+                    solution = level1.solve2();
+                    break;
+                default:
+                    solution = level1.solve1();
+            }
             break;
         case '02':
-            solution1 = level2.solve1();
-            solution2 = level2.solve2();
+            switch (subLevel) {
+                case '2':
+                    solution = level2.solve2();
+                    break;
+                default:
+                    solution = level2.solve1();
+            }
+            break;
+        case '03':
+            switch (subLevel) {
+                case '2':
+                    solution = level3.solve2();
+                    break;
+                default:
+                    solution = level3.solve1();
+            }
             break;
         default:
             break;
     }
 
-    res.render('solution', { lvl: level, solution1: solution1, solution2: solution2 });
+    res.render('solution', {
+        lvl: level,
+        subLvl: subLevel,
+        solution: solution
+    });
 });
 
 module.exports = router;
