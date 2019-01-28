db5a281df95db28b7d3e58bb1ac23e659a7ba81b
Werner van Aswegen <wernerva@users.noreply.github.com>
2019-01-28 09:10:13 +0200

    Updated index route maxDay to 8

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,7 +14,7 @@ export class IndexRoute extends BaseRoute {
 
     public index(req: Request, res: Response, next: NextFunction) {
         // this.render(req, res, 'index');
-        res.render('index', { maxDay: 7 });
+        res.render('index', { maxDay: 8 });
 
         if (next) {
             next();
